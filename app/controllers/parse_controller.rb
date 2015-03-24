ce36329031757bbb29b7940b989ece702dd721f3
arlyxiao <kingla_pei@163.com>
2015-03-24 15:38:12 +0800

    refactor

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -10,47 +10,63 @@ class ParseController < ApplicationController
 
 
   def create
-    file = Tempfile.new('foo')
-    # p file.path
-    file.write(params[:chat_text])
-    file.rewind
-    # file.read
-    file.close
+    begin
+      tmp_file = build_tmp_file
+      chat = parse_chat_file(tmp_file)
 
-    # p file.path
-    # p '===='
-    
-    first_line = File.open(file.path) {|f| f.readline}
-    if (first_line =~ /^【/)
-      chat = ImChatParser.load(file.path, 'multiple')
-    else
-      chat = ImChatParser.load(file.path, 'single')
+      save_into_db(chat)
+
+      redirect_to '/'
+    rescue Exception => e
+      p e.message
+      render :nothing => true
     end
-
-    file.unlink
-
-    # p chat.lines.length
-    # p chat.users
-    # p chat.lines[2].user
-    # p chat.lines[2].text
-
-    chat_record = ChatRecord.create
-
-    chat.lines.each do |line|
-      user = User.where(
-        :name_list => line.user.names.map {|item| item }.join(','),
-        :qq_num => line.user.qq_num
-      ).first_or_create!
-
-      chat_record.lines.create(
-        :user_id => user.id,
-        :text => line.text,
-        :time => line.time
-      )
-    end
-
-    redirect_to '/'
-    # render :nothing => true
   end
+
+
+  private
+    def build_tmp_file
+      file = Tempfile.new('foo')
+      # p file.path
+      file.write(params[:chat_text])
+      file.rewind
+      # file.read
+      file.close
+
+      # p file.path
+      # p '===='
+      file
+    end
+
+    def parse_chat_file(file)
+      file = build_tmp_file
+
+      first_line = File.open(file.path) {|f| f.readline}
+      if (first_line =~ /^【/)
+        chat = ImChatParser.load(file.path, 'multiple')
+      else
+        chat = ImChatParser.load(file.path, 'single')
+      end
+
+      file.unlink
+      
+      chat
+    end
+
+    def save_into_db(chat)
+      chat_record = ChatRecord.create
+      chat.lines.each do |line|
+        user = User.where(
+          :name_list => line.user.names.map {|item| item }.join(','),
+          :qq_num => line.user.qq_num
+        ).first_or_create!
+
+        chat_record.lines.create(
+          :user_id => user.id,
+          :text => line.text,
+          :time => line.time
+        )
+      end
+    end
 
 end
