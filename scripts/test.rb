fef4d9b7577e3437d2c882bf0008200cf2b9feaa
arlyxiao <kingla_pei@163.com>
2015-03-26 15:01:38 +0800

    add

--- scripts/test.rb
+++ scripts/test.rb
@@ -2,10 +2,17 @@ require 'im-chat-parser'
 
 
 file = '/Users/xiaorich/www/mindpin/im-chat-parser-demo/scripts/test.text'
-chat = ImChatParser.load(file, 'single')
+chat = ImChatParser.load(file)
 
 
 p chat.users
 p chat.lines
+p '======='
 p chat.lines[1].user
 p chat.lines[1].user.qq_num
+p chat.lines[1].time
+p chat.lines[1].text
+
+
+p '======='
+p chat.lines.last.text
